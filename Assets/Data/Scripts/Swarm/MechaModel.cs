75300794e7c44e146a3871913f4a3f26dad3315b
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2017-12-09 22:01:07 +0100

    New mecha mesh, digital clock font

--- Assets/Data/Scripts/Swarm/MechaModel.cs
+++ Assets/Data/Scripts/Swarm/MechaModel.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Framework;
 using UnityEngine;
 
 public class MechaModel : MonoBehaviour
 {
     public float RotateSpeed = 2;
+    
+    [Header("Refs")]
+    public Animator Anim;
+    public StatePawn Pawn;
+
+    [Header("Anims")] 
+    public string Forward;
+    public string Side;
 
     private Vector3 oldForward;
 
@@ -13,15 +22,31 @@ public class MechaModel : MonoBehaviour
         oldForward = transform.forward;
     }
 
+    public void Update()
+    {
+        if (!Pawn || !Anim)
+            return;
+
+        var lookDir  = transform.forward;
+        var rotation = Quaternion.FromToRotation(Vector3.forward, new Vector3(lookDir.x * -1, lookDir.y, lookDir.z));
+        Vector3 movement = rotation * Pawn.Velocity;
+        
+        if (!string.IsNullOrEmpty(Forward))
+            Anim.SetFloat(Forward, movement.z / Pawn.Movement.MaxSpeed);
+
+        if (!string.IsNullOrEmpty(Side))
+            Anim.SetFloat(Side, movement.x / Pawn.Movement.MaxSpeed);
+    }
+
     public void SetDirection(Vector3 direction)
     {
-        if (direction.magnitude > 0.01f)
+        if (direction.magnitude > 0.1f)
         {
-            var target = Vector3.Slerp(oldForward, direction, Time.deltaTime * RotateSpeed);
+            var target   = Vector3.Slerp(oldForward, direction, Time.deltaTime * RotateSpeed);
                 target.y = 0;
             
             transform.forward = target;
-            oldForward = target;
+            oldForward        = target;
         }
     }
 }
